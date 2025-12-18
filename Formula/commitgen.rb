8e578c732b60131941ee72d079220c0ab7685fac
Lake-Millie-DevOps <lake@millie.town>
2025-12-18 18:20:12 +0900

    chore: update commitgen to v0.1.1
    Changes in v0.1.1:
    - Add rate limit error detection and handling
    - Automatically summarize large diffs (>10KB)
    - Reduce default max_tokens to 100
    - Add diff size warnings
    - Improve error messages for rate limits

--- Formula/commitgen.rb
+++ Formula/commitgen.rb
@@ -1,26 +1,26 @@
 class Commitgen < Formula
   desc "AI-powered Git commit message generator"
   homepage "https://github.com/leehosu/commitgen"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/leehosu/commitgen/releases/download/v0.1.0/commitgen_0.1.0_Darwin_arm64.tar.gz"
-      sha256 "a42048e177ed63b919245ea3f2ac40e0e5ae51fadbd07c3e73fff32a12a26e22"
+      url "https://github.com/leehosu/commitgen/releases/download/v0.1.1/commitgen_0.1.1_Darwin_arm64.tar.gz"
+      sha256 "723d0e2adf338590f5078be4aaa46d1271a1d72e3b2d4c017935539f42685bb9"
     elsif Hardware::CPU.intel?
-      url "https://github.com/leehosu/commitgen/releases/download/v0.1.0/commitgen_0.1.0_Darwin_x86_64.tar.gz"
-      sha256 "b9154c9a4df88dd2d9fdc18a24d36c9db31588599a860e070b9d12a4e67fdc38"
+      url "https://github.com/leehosu/commitgen/releases/download/v0.1.1/commitgen_0.1.1_Darwin_x86_64.tar.gz"
+      sha256 "87b9d62f3419d49336619dd9cabb5d2d4898d6c3b4243d85a12c7bce1c045b9d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/leehosu/commitgen/releases/download/v0.1.0/commitgen_0.1.0_Linux_arm64.tar.gz"
-      sha256 "a1f84def45654d0bbb63d698494cb771547370e7b5c0922dfd90e1daf04ab557"
+      url "https://github.com/leehosu/commitgen/releases/download/v0.1.1/commitgen_0.1.1_Linux_arm64.tar.gz"
+      sha256 "88b44b42ca02d76eb3c0223d9c62a0ac6848cd4e7c05a77083f1c81aef2bd8dc"
     elsif Hardware::CPU.intel?
-      url "https://github.com/leehosu/commitgen/releases/download/v0.1.0/commitgen_0.1.0_Linux_x86_64.tar.gz"
-      sha256 "24784f7233a3c10b5c5c76f8fa22eccda5fab5f56d5f83155515ee1fb6c80707"
+      url "https://github.com/leehosu/commitgen/releases/download/v0.1.1/commitgen_0.1.1_Linux_x86_64.tar.gz"
+      sha256 "95d8e86cb00373ed52ed1bb28e125f7af94a763e3db79bcdc6bef105a5ce4556"
     end
   end
 
